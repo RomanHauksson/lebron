abeec3195115f7c71faac50fe74bdcea154662cf
Roman Hauksson <62364808+RomanHN@users.noreply.github.com>
2023-04-15 17:56:56 -0500

    setting up chatbot MVP

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import Chatbot from "react-chatbot-kit";
+import 'react-chatbot-kit/build/main.css';
+
+import config from './chatbot/config.js';
+import MessageParser from './chatbot/MessageParser.jsx';
+import ActionProvider from './chatbot/ActionProvider.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chatbot
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />
     </div>
   );
 }
